c249fcc6bdd48bccf4a7aa044ae69ea846817f6b
German Voytekhovich <ueuknjg@gmail.com>
2020-01-22 14:38:41 +0300

    initial commit

---
+++ webpack.base.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const paths = {
+	src: path.join(process.cwd(), 'src'),
+	build: path.join(process.cwd(), 'dist'),
+};
+
+module.exports = {
+	externals: {
+		paths,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+						options: {
+							transpileOnly: true,
+							logLevel: 'info',
+						},
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		]
+	},
+	resolve: {
+		modules: ['node_modules', 'src'],
+		extensions: ['.ts', '.js'],
+		mainFields: ['browser', 'jsnext:main', 'main'],
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin({
+			tslint: false,
+			useTypescriptIncrementalApi: true,
+			checkSyntacticErrors: true,
+		}),
+	],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const baseConfig = require('./webpack.base');
+
+const devConfig = merge(baseConfig, {
+	mode: 'development',
+	entry: [
+		`${baseConfig.externals.paths.src}/index.ts`,
+	],
+	output: {
+		path: `${baseConfig.externals.paths.build}`,
+		filename: '[name].js',
+		chunkFilename: '[name].chunk.js',
+		publicPath: '/',
+	},
+	devtool: 'cheap-module-eval-source-map',
+	optimization: {},
+	devServer: {
+		port: 8080,
+		overlay: {
+			warnings: true,
+			errors: true,
+		},
+		contentBase: baseConfig.externals.paths.src,
+		watchContentBase: true,
+		disableHostCheck: true,
+		stats: {
+			modules: false,
+			warnings: true,
+			chunks: false,
+			publicPath: false,
+			hash: false,
+			version: false,
+			timings: true,
+		},
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(), // Tell webpack we want hot reloading
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: 'src/index.html',
+		}),
+		new CircularDependencyPlugin({
+			exclude: /a\.js|node_modules/, // exclude node_modules
+			failOnError: false, // show a warning when there is a circular dependency
+		}),
+	],
+});
+
+module.exports = new Promise((resolve) => resolve(devConfig));
